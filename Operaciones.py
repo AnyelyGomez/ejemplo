c06558ac0b5ddeaf7f2e5b1bf920062e99d5d78c
AnyelyGomez <anyelitagn@gmail.com>
2019-08-28 10:20:22 -0500

    v3

---
+++ Operaciones.py
@@ -0,0 +1,36 @@
+#----------------------------------------------------------------
+# hacer calculadora
+
+def suma(a,b):
+    resultado = a+b
+    print("La suma es: ", resultado)
+
+#convertir en int
+
+a= int(input("Digite a: "))
+b= int(input("Di5gite b: "))
+suma(a,b)
+
+#----------------------------------------------------------------
+# hacer calculadora
+
+def resta(a,b):
+    resultado = a-b
+    print("La resta es: ", resultado)
+
+#convertir en int
+a= int(input("Digite a: "))
+b= int(input("Digite b: "))
+resta(a,b)
+
+#----------------------------------------------------------------
+# hacer calculadora
+
+def mult(a,b):
+    resultado = a*b
+    print("La multiplicación es: ", resultado)
+
+#convertir en int
+a= int(input("Digite a: "))
+b= int(input("Digite b: "))
+mult(a,b)
---
+++ ejemplos.py
@@ -0,0 +1,38 @@
+print ("Hola mundo")
+# variables 
+#int
+#float
+#srt
+#bool
+
+x=2
+print (type(x))
+
+y=0.5
+print (type(y))
+
+z= "Hola Mundo"
+print (type(z))
+
+#funciòn
+
+def persona(nombre, apellido, edad):
+   # print("Hola", anyely, "su apellido es:", apellido, "Mi edad es:", edad)
+    print("Hola", nombre, "su apellido es:", apellido, "Mi edad es:", edad)
+
+#llamar funcion
+#persona("Anyely", "Gomez", "24")
+
+#pedir nombre por consola
+nombre   = input("Digite su nombre: ")
+apellido = input("Digite su apellido: ")
+edad     = input("Digite su edad: ")   
+
+persona(nombre, apellido, edad)
+
+
+
+
+
+
+
